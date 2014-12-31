a73c471242353aab2a2e6e14dee6dfe78f5de565
trimm <robertjessecooke@gmail.com>
2014-12-31 16:01:01 +0100

    cc

--- core9-ui-widgets/widgets/test-editor/child/tabs/system.js
+++ core9-ui-widgets/widgets/test-editor/child/tabs/system.js
@@ -1,7 +1,28 @@
 console.log("exec loaded system script..");
 
-$('#backup-btn').on('click', function(){
-	console.log(this);
-	console.log("store");
-	console.log(store);
-});
+$('#backup-btn').on(
+		'click',
+		function() {
+			console.log(this);
+			console.log("store");
+			console.log(store);
+
+			var user = store.get('user')
+
+			console.log("user : " + user);
+
+			if (typeof user === "undefined") {
+
+				var person = prompt("Please enter your name", "Harry Potter");
+				if (person != null) {
+					store.set('user', person)
+				}
+				
+				var password = prompt("Please enter your password", "Harry Potter");
+				if (password != null) {
+					store.set('password', password)
+				}
+
+			}
+
+		});
