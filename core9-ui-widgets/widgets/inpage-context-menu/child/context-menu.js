699c72aed2adf2f2a44b8b4caadb552813f78a96
trimm <robertjessecooke@gmail.com>
2015-01-23 14:51:36 +0100

    cc

--- core9-ui-widgets/widgets/inpage-context-menu/child/context-menu.js
+++ core9-ui-widgets/widgets/inpage-context-menu/child/context-menu.js
@@ -22,7 +22,7 @@ Core9.menu = {
 			menu += '<li><button style="border: 0 rgba(0,0,0,0); background-color: transparent; width: 125px; text-decoration: none; color:#000; text-align:left;"  onclick="Core9.menu.insertAfterBlock()">{insertAfter}</button></li>';
 			menu += '<!--<li><button style="border: 0 rgba(0,0,0,0); background-color: transparent; width: 125px; text-decoration: none; color:#000; text-align:left;"  onclick="if (\'parentIFrame\' in window) window.parentIFrame.sendMessage(\'edit-block\');return false;">{switchblock}</button></li>-->';
 			menu += '<li><button style="border: 0 rgba(0,0,0,0); background-color: transparent; width: 125px; text-decoration: none; color:#000; text-align:left;"  onclick="Core9.menu.deleteBlock()">{removeblock}</button></li>';
-			menu += '<li><button style="border: 0 rgba(0,0,0,0); background-color: transparent; width: 125px; text-decoration: none; color:#000; text-align:left;"  onclick="Core9.menu.testData()">{testdata}</button></li>';
+			//menu += '<li><button style="border: 0 rgba(0,0,0,0); background-color: transparent; width: 125px; text-decoration: none; color:#000; text-align:left;"  onclick="Core9.menu.testData()">{testdata}</button></li>';
 			menu += '</ul>';
 			return menu;
 		},
@@ -151,7 +151,7 @@ Core9.menu = {
 				insertAfter : "insert after",
 				switchblock : "verwissel",
 				removeblock : "verwijder",
-				testdata : "A/B test",
+				//testdata : "A/B test",
 				x : x,
 				y : y
 			});
